7e4b09df8a432ef708ab17c9eac156458db2d7c3
Jackson Garcia <jacksongarciajr@gmail.com>
2024-11-06 17:23:45 -0300

    Initi commit

---
+++ src/FInput/index.tsx
@@ -0,0 +1,120 @@
+import { Feather } from "@expo/vector-icons"
+import { useEffect, useState } from "react"
+import {
+  Dimensions,
+  Keyboard,
+  Platform,
+  StyleSheet,
+  TextInput,
+  View,
+} from "react-native"
+import Animated, {
+  useAnimatedKeyboard,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated"
+
+export default function FInput() {
+  const [isFocus, setFocus] = useState(false)
+  const [value, setValue] = useState("")
+
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener("keyboardDidShow", () => {
+      width.value = withTiming(Dimensions.get("window").width - 26, {
+        duration: 200,
+      })
+    })
+    const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {
+      width.value = withTiming(
+        Platform.OS != "web"
+          ? Dimensions.get("window").width - (45 + 36)
+          : Dimensions.get("window").width > 767
+          ? 330
+          : Dimensions.get("window").width - (45 + 36),
+        {
+          duration: 200,
+        }
+      )
+    })
+
+    return () => {
+      showSubscription.remove()
+      hideSubscription.remove()
+    }
+  }, [])
+
+  const keyboard = useAnimatedKeyboard()
+  const width = useSharedValue<number>(
+    Platform.OS != "web"
+      ? Dimensions.get("window").width - (45 + 36)
+      : Dimensions.get("window").width > 767
+      ? 330
+      : Dimensions.get("window").width - (45 + 36)
+  )
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateY: -keyboard.height.value }],
+  }))
+
+  return (
+    <Animated.View
+      style={[
+        {
+          width,
+        },
+        animatedStyles,
+      ]}
+    >
+      <View style={styles.icon}>
+        <Feather
+          name='search'
+          size={21}
+          color={isFocus || value.length > 0 ? "#00436a" : "#E8EDF2"}
+        />
+      </View>
+      <TextInput
+        style={[
+          styles.input,
+          {
+            borderColor: isFocus || value.length > 0 ? "#00436a" : "#E8EDF2",
+          },
+        ]}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        onSubmitEditing={Keyboard.dismiss}
+        returnKeyType='done'
+        onChangeText={setValue}
+        value={value}
+      />
+    </Animated.View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    borderRadius: 10,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#dadfe4",
+    elevation: 0.3,
+    backgroundColor: "white",
+    height: 45,
+    color: "#2C698D",
+    fontSize: 14,
+    fontStyle: "normal",
+    alignContent: "center",
+    paddingLeft: 41,
+    paddingRight: 13,
+  },
+  icon: {
+    position: "absolute",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    marginLeft: 12,
+    width: 21,
+    bottom: 0,
+    zIndex: 9999999,
+  },
+})
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as FInput } from './FInput';
