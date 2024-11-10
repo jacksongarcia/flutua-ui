8522853cebf2040bab33f4fffa53bdf15c34eb45
Jackson Garcia <jacksongarciajr@gmail.com>
2024-11-09 23:35:17 -0300

    Adicionando no index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import 'react-native-reanimated';
 import FInput from './FInput';
 import { FMenu } from './FMenu';
+import FButton from './FButton';
+import FModal from './FModal';
+import useModal from './hooks/modal'
+import useMenu from './hooks/menu'
 
 export {
   FInput,
-  FMenu
+  FMenu,
+  FButton,
+  FModal,
+
+  useModal,
+  useMenu
 }
